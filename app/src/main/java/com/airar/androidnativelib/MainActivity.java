a4ab1bd8a1ae79d5fef2228d677c33b72ef75925
Guseul <jgs@airar.co>
2021-04-23 18:44:35 +0900

    [2021.04.23] os 11 대응

--- app/src/main/java/com/airar/androidnativelib/MainActivity.java
+++ app/src/main/java/com/airar/androidnativelib/MainActivity.java
@@ -7,10 +7,9 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -26,13 +25,9 @@ import com.arthenica.mobileffmpeg.Signal;
 import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerActivity;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import androidx.core.content.FileProvider;
 
@@ -45,9 +40,12 @@ public class MainActivity
     private String unityGameObjectname;
     private String methodName;
 
+    private String appName;
+    private String strPermission;
+
+
     @TargetApi(23)
     public void CallOnResume() {
-        //
         Log.i("Unity", "callonresume");
         this.mUnityPlayer.resume();
     }
@@ -78,75 +76,110 @@ public class MainActivity
         }
     };
 
-    // 갤러리에 사진 저장하기
-    public void saveImageToGallery(byte[] imgFile, String fileName, String gameObjectName,
+    /*
+     * MediaStore 클래스를 사용하여 이미지 저장하기
+     * */
+    public void saveImageWithMediaStore(byte[] fileByte, String fileName, String gameObjectName,
             String methodName) {
+        int resultCode;
+
         ContentValues values = new ContentValues();
         values.put(MediaStore.Images.Media.DISPLAY_NAME, fileName);
         values.put(MediaStore.Images.Media.MIME_TYPE, "image/*");
-        // 파일을 write중이라면 다른곳에서 데이터요구를 무시하겠다는 의미입니다.
-        values.put(MediaStore.Images.Media.IS_PENDING, 1);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.IS_PENDING, 1);
+        }
 
         ContentResolver contentResolver = getContentResolver();
-        Uri collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
-        Uri item = contentResolver.insert(collection, values);
+        Uri item = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
         try {
-            ParcelFileDescriptor pdf = contentResolver.openFileDescriptor(item, "w", null);
-            if (pdf == null) {
+            ParcelFileDescriptor pfd = contentResolver.openFileDescriptor(item, "w", null);
 
+            if (pfd == null) {
+                resultCode = 2;
+                Log.d(TAG, "null");
             } else {
-                InputStream inputStream = getImageInputStram(
-                        BitmapFactory.decodeByteArray(imgFile, 0, imgFile.length));
-                byte[] strToByte = getBytes(inputStream);
-                FileOutputStream fos = new FileOutputStream(pdf.getFileDescriptor());
-                fos.write(strToByte);
+                FileOutputStream fos = new FileOutputStream(pfd.getFileDescriptor());
+                fos.write(fileByte);
                 fos.close();
-                inputStream.close();
-                pdf.close();
-                contentResolver.update(item, values, null, null);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    values.clear();
+                    values.put(MediaStore.Images.Media.IS_PENDING, 0);
+                    contentResolver.update(item, values, null, null);
+                }
+
+                resultCode = 1;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
+            resultCode = 2;
             e.printStackTrace();
         }
-        values.clear();
-        // 파일을 모두 write하고 다른곳에서 사용할 수 있도록 0으로 업데이트를 해줍니다.
-        values.put(MediaStore.Images.Media.IS_PENDING, 0);
-        contentResolver.update(item, values, null, null);
 
         this.unityGameObjectname = gameObjectName;
         this.methodName = methodName;
 
         Message msg2 = new Message();
-        msg2.what = 1;
+        msg2.what = resultCode;
         this.mHandler.sendMessage(msg2);
     }
 
-    private InputStream getImageInputStram(Bitmap bmp) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, bytes);
-        byte[] bitmapData = bytes.toByteArray();
-        ByteArrayInputStream bs = new ByteArrayInputStream(bitmapData);
+    /*
+     * MediaStore 클래스를 사용하여 비디오 저장하기
+     * */
+    public void saveVideoWithMediaStore(byte[] fileByte, String fileName, String gameObjectName,
+            String methodName) {
+        int resultCode;
 
-        return bs;
-    }
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Video.Media.DISPLAY_NAME, fileName);
+        values.put(MediaStore.Video.Media.MIME_TYPE, "video/*");
 
-    public byte[] getBytes(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-        int bufferSize = 1024;
-        byte[] buffer = new byte[bufferSize];
-
-        int len = 0;
-        while ((len = inputStream.read(buffer)) != -1) {
-            byteBuffer.write(buffer, 0, len);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Video.Media.IS_PENDING, 1);
         }
-        return byteBuffer.toByteArray();
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri item = contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
+
+        try {
+            ParcelFileDescriptor pfd = contentResolver.openFileDescriptor(item, "w", null);
+
+            if (pfd == null) {
+                resultCode = 2;
+                Log.d(TAG, "null");
+            } else {
+                FileOutputStream fos = new FileOutputStream(pfd.getFileDescriptor());
+                fos.write(fileByte);
+                fos.close();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    values.clear();
+                    values.put(MediaStore.Video.Media.IS_PENDING, 0);
+                    contentResolver.update(item, values, null, null);
+                }
+
+                resultCode = 1;
+            }
+        } catch (IOException e) {
+            resultCode = 2;
+            e.printStackTrace();
+        }
+
+        this.unityGameObjectname = gameObjectName;
+        this.methodName = methodName;
+
+        Message msg2 = new Message();
+        msg2.what = resultCode;
+        this.mHandler.sendMessage(msg2);
     }
 
 
-    // 갤러리에 파일 추가하기
+    /*
+     *  갤러리에 파일 추가하기
+     * */
     private void galleryAddPic(String currentPhotoPath) {
         File f = new File(currentPhotoPath);
         Uri contentUri = Uri.fromFile(f);
@@ -156,7 +189,9 @@ public class MainActivity
         sendBroadcast(mediaScanIntent);
     }
 
-    // 갤러리에 사진 추가하기
+    /*
+     * 갤러리에 사진 추가하기
+     * */
     public void addImageToGallery(String path, String gameObjectName, String methodName) {
         File file = new File(path);
         if (file.exists()) {
@@ -178,7 +213,9 @@ public class MainActivity
         }
     }
 
-    // 갤러리에 비디오 추가하기
+    /*
+     * 갤러리에 비디오 추가하기
+     * */
     public void addVideoToGallery(String path, String gameObjectName, String methodName) {
         File file = new File(path);
         if (file.exists()) {
@@ -200,7 +237,9 @@ public class MainActivity
         }
     }
 
-    // 비디오 회전하기
+    /*
+     *  비디오 회전하기
+     * */
     public void rotateVideo(String path, String gameObjectName, String methodName) {
         String[] objPath = path.split(";");
         String videoPath = objPath[0];
@@ -219,7 +258,7 @@ public class MainActivity
         String cmd = "-i " + videoPath + " -map_metadata 0 -metadata:s:v rotate=\"" + _rotate
                 + "\" -codec copy " + OutputPath;
 
-        Log.i("Unity", cmd);
+        Log.i(TAG, cmd);
 
 
         this.unityGameObjectname = gameObjectName;
@@ -249,17 +288,20 @@ public class MainActivity
         }
     }
 
+    /*
+     * 이미지 공유하기 - 2021.04.23 기준 쓰이지 않음
+     * */
     public void ShareImage(String imgPath) {
         File files = new File(imgPath);
         if (!files.exists()) {
             return;
         }
+
         Intent intentSend = new Intent("android.intent.action.SEND");
         intentSend.setType("image/*");
 
         //Uri uri = FileProvider.getUriForFile(this, "com.airar.trickeye.provider", new File
         // (imgPath));
-
 
         Uri contentUri = FileProvider.getUriForFile(getApplicationContext(),
                 "com.airar.androidnativelib.fileprovider", files);
@@ -274,6 +316,9 @@ public class MainActivity
         startActivity(Intent.createChooser(intentSend, "공유하기"));
     }
 
+    /*
+     *  비디오 공유하기 - 2021.04.23 기준 쓰이지 않음
+     * */
     public void ShareVideo(String imgPath) {
         Intent intentSend = new Intent("android.intent.action.SEND");
         intentSend.setType("video/*");
@@ -290,6 +335,9 @@ public class MainActivity
         startActivity(Intent.createChooser(intentSend, "공유하기"));
     }
 
+    /*
+     * 텍스트 공유하기 - 2021.04.23 기준 쓰이지 않음
+     * */
     public void ShareText(String linkUrl) {
         Intent intentSend = new Intent("android.intent.action.SEND");
 
@@ -343,16 +391,24 @@ public class MainActivity
     }
 
 
-    // 갤러리 이동
+    /*
+     * 갤러리 앱 열기
+     * */
     public void OpenGallery() {
-        Intent intent = new Intent(Intent.ACTION_VIEW,
+        Intent intent = Intent.makeMainSelectorActivity(Intent.ACTION_MAIN,
+                Intent.CATEGORY_APP_GALLERY);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        /*Intent intent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse("content://media/internal/images/media"));
-        intent.setType("image/*");
+        intent.setType("image/*");*/
         startActivity(intent);
 
     }
 
-    // 퍼미션 체크하기
+    /*
+     * 퍼미션 체크하기
+     * */
     public void CheckPermission(String _permissionStr, String gameObjectName,
             String gameMethodName) {
         this.unityGameObjectname = gameObjectName;
@@ -376,11 +432,16 @@ public class MainActivity
         return 0;
     }
 
-    // 퍼미션 호출하기
+    /*
+     * 퍼미션 호출하기
+     * */
     public void CallPermission(String _permissionStr, String gameObjectName,
-            String gameMethodName) {
+            String gameMethodName, String appName) {
         this.unityGameObjectname = gameObjectName;
         this.methodName = gameMethodName;
+
+        this.appName = appName;
+
         if (Build.VERSION.SDK_INT < 23) {
             SendRequestResultToUnity("OnVersion");
         } else {
@@ -390,24 +451,61 @@ public class MainActivity
 
     @TargetApi(23)
     public void requestPermission(String permissionStr) {
-        UnityPlayer.currentActivity.requestPermissions(new String[]{permissionStr}, 0);
+        SharedPreferences sf = getSharedPreferences(this.appName, MODE_PRIVATE);
+        boolean donotAskagain = sf.getBoolean(permissionStr, false);
+
+        if (donotAskagain) {
+            strPermission = permissionStr;
+
+            Intent i = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(i, 100, null);
+        } else {
+            UnityPlayer.currentActivity.requestPermissions(new String[]{permissionStr}, 0);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+
+            if (hasPermission(strPermission) == 0) {
+                Log.i(TAG, "PermissionDenied");
+                SendRequestResultToUnity("PermissionDenied");
+            } else {
+                Log.i(TAG, "PermissionAllow");
+                SendRequestResultToUnity("PermissionAllow");
+            }
+
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @TargetApi(23)
     public void onRequestPermissionsResult(int requestCode, String[] permissions,
             int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 0:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    SendRequestResultToUnity("PermissionAllow");
-                    //restartApp(this.p_myContext);
-                } else if (shouldShowRequestPermissionRationale(permissions[0])) {
+
+        if (requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                SendRequestResultToUnity("PermissionAllow");
+                //restartApp(this.p_myContext);
+            } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                if (shouldShowRequestPermissionRationale(permissions[0])) {
                     SendRequestResultToUnity("PermissionDenied");
                 } else {
+                    SharedPreferences sharedPreferences = getSharedPreferences(this.appName,
+                            MODE_PRIVATE);
+
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(permissions[0], true);
+                    editor.apply();
+
                     SendRequestResultToUnity("PermissionDenied");
                 }
-                break;
+            }
         }
     }
 
